ace58cfd8130073f3d064d698d89f43f32eea353
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2017-05-12 01:22:11 +1000

    fixed pivot table to produce correct results lol

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ import numpy as np
 app = Flask(__name__)
 df = pd.read_csv("Data/vgsales.csv")
 
+aggFunctions = {'count':np.count_nonzero, 'sum':np.sum, 'avg':np.mean,
+            'min':np.min, 'max':np.max, 'med':np.median}
+
 @app.route('/')
 def index():
     return 'This is the homepage'
@@ -29,10 +32,10 @@ def pivot():
         cat2 = request.form['cat2']
         aggr = request.form['aggr']
         filter = request.form['filter']
-        if str(aggr) == 'All':
-            table = pd.pivot_table(df, index=[str(cat1), str(cat2)], values=["NA_Sales","EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"], aggfunc=[np.sum])
+        if str(filter) == 'All':
+            table = pd.pivot_table(df, index=[str(cat1), str(cat2)], values=["NA_Sales","EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"], aggfunc=aggFunctions[aggr])
         else:
-            table = pd.pivot_table(df,index=[str(cat1),str(cat2)], values=[str(aggr)],aggfunc=[np.sum] )
+            table = pd.pivot_table(df,index=[str(cat1)], columns=[str(cat2)], values=[str(filter)],aggfunc=aggFunctions[aggr] )
     return render_template("pivot.html", table=table.to_html())
 
 
