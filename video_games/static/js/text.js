d6c3ce5740a39de4d6a04740e8cc20b2110cd581
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2017-05-24 15:00:48 +1000

    Working chart1 and 2

---
+++ video_games/static/js/text.js
@@ -0,0 +1,13 @@
+var text= {
+    "Year": "The best year to release game is 2008 with highest Global Sales. " +
+            "However, the year with the highest mean unit sales is 1989. " + 
+            "Though total sales have decreased in recent year, " + 
+            "mean unit sales per game has been relatively constant." 
+    , "Genre": "The most prolific genre is Action. " + 
+            "However, the genre with highest average sales is Platform."
+    , "Publisher": "While Electronic Arts make the most game, " + 
+                "Nintendo earns the most from the game market, " +
+                "but Palcom has the highest average sales."
+    , "Platform": "PS2 has the most games released and highest global sales. " +
+                "GB has the highest average sales" 
+};
