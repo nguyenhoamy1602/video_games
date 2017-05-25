9bf9dc88a97437eb3e853523f85fd7da727b3f33
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2017-05-26 09:23:53 +1000

    added comments to convert

--- video_games/convert.py
+++ video_games/convert.py
@@ -1,43 +1,44 @@
+#changes the pandas CSV ver of pivot table into arrays that highcharts can read
 def convertCSVFormat(file, cat1, cat2):
     lines = file.split('\n')
     aggr = True
-    value = True
-    yLabels = []
     xLabels = []
-    values = []
+    yLabels = []
+    label = True
+    data = []
     x = 0
     for line in lines:
-        items = line.split(',')
+        list = line.split(',')
         if aggr:
             aggr = False
         else:
-            if value:
-                value = False
-                for item in items[1:]:
+            if label:
+                label = False
+                for item in list[1:]:
                     if str(cat2) == 'Year':
                         item = int(float(item))
                         yLabels.append(str(item))
                     else:
                         yLabels.append(item)
             else:
-                if str(cat1) == 'Year' and cat1 != items[0]:
+                if str(cat1) == 'Year' and cat1 != list[0]:
                     try:
-                        item = int(float(items[0]))
+                        item = int(float(list[0]))
                         xLabels.append(str(item))
                     except ValueError:
-                        xLabels.append(items[0])
-                elif cat1 != items[0]:
-                    xLabels.append(items[0])
+                        xLabels.append(list[0])
+                elif cat1 != list[0]:
+                    xLabels.append(list[0])
                 y = 0
-                if cat1 != items[0]:
-                    for item in items[1:]:
+                if cat1 != list[0]:
+                    for item in list[1:]:
                         try:
                             item = float(item)
-                            values.append([x, y, round(item,2)])
+                            data.append([x, y, round(item,2)])
                         except ValueError:
-                            values.append([x, y, 0])
+                            data.append([x, y, 0])
                         y += 1
                     x += 1
 
 
-    return(xLabels, yLabels, values)
+    return(xLabels, yLabels, data)
